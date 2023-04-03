b24e7483d10598b27a60afd9daa8966969845c45
blub <minhanpark882@gmail.com>
2023-04-03 11:01:18 -0500

    Added two boxes to TUI and also made it so that it exits cleanly when user presses q

--- src/menu.rs
+++ src/menu.rs
@@ -1,27 +1,35 @@
 use ratatui::{
     backend::Backend,
     layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders},
+    widgets::{Block, Borders, Paragraph, Wrap},
+    text::{Spans},
     Frame,
 };
+ 
+
 pub fn ui<B: Backend>(f: &mut Frame<B>) {
    let chunks = Layout::default()
-        .direction(Direction::Vertical)
+        .direction(Direction::Horizontal)
         .margin(1)
         .constraints(
             [
                 Constraint::Percentage(10),
                 Constraint::Percentage(80),
-                Constraint::Percentage(10)
             ].as_ref()
         )
         .split(f.size());
     let block = Block::default()
-         .title("Block")
+         .title("Menu")
          .borders(Borders::ALL);
     f.render_widget(block, chunks[0]);
+    
+    let text = vec![
+        Spans::from("Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."),
+    ];
+
     let block = Block::default()
-         .title("Block 2")
+         .title("Variable Name")
          .borders(Borders::ALL);
-    f.render_widget(block, chunks[1]);
+    let paragraph = Paragraph::new(text).block(block).wrap(Wrap {trim:true});
+    f.render_widget(paragraph, chunks[1]);
 }
